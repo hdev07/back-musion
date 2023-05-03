e60143650c370cf531fba9b5ec880a5dc459ad87
Alejandro Cruz <hcruz0716@gmail.com>
2023-05-03 12:46:30 -0600

    First steps

---
+++ database/connectdb.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+
+const uri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`;
+
+try {
+  await mongoose.connect(uri);
+  console.log("Connected to DB successfully");
+} catch (error) {
+  console.log("Error connecting to DB: ", error);
+}
---
+++ routes/auth.route.js
@@ -0,0 +1,46 @@
+import express from "express";
+import { login, register } from "../controllers/auth.controller.js";
+import { body } from "express-validator";
+import { validationResultExpress } from "../middlewares/validationResultExpress.js";
+
+const router = express.Router();
+
+router.post(
+  "/register",
+  [
+    body("email", "Formato de email incorrecto")
+      .trim()
+      .isEmail()
+      .normalizeEmail(),
+    body("password", "Password debe tener al menos 8 caracteres")
+      .trim()
+      .isLength({ min: 8 }),
+    body("password", "Formato de password incorrecto").custom(
+      (value, { req }) => {
+        if (value !== req.body.repassword) {
+          throw new Error("Passwords no coinciden");
+        }
+        return value;
+      }
+    ),
+  ],
+  validationResultExpress,
+  register
+);
+
+router.post(
+  "/login",
+  [
+    body("email", "Formato de email incorrecto")
+      .trim()
+      .isEmail()
+      .normalizeEmail(),
+    body("password", "Password debe tener al menos 8 caracteres")
+      .trim()
+      .isLength({ min: 8 }),
+  ],
+  validationResultExpress,
+  login
+);
+
+export default router;
