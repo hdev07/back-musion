579f259f846d6805460cb36b9b84d06c34c8e6de
Alejandro Cruz <hcruz0716@gmail.com>
2023-06-14 11:26:00 -0600

    Update Museum

--- controllers/museum.controller.js
+++ controllers/museum.controller.js
@@ -146,71 +146,21 @@ export const createMuseums = async (req, res) => {
 export const updateMuseumById = async (req, res) => {
   try {
     const { id } = req.params;
-    const {
-      name,
-      description,
-      image,
-      category,
-      coordinates,
-      address,
-      telephone,
-      url,
-      openingHours,
-      priceRange,
-      review,
-      travelTime,
-      stayTime,
-    } = req.body;
+    const updatedData = req.body;
 
-    const museum = await Museum.findById(id);
+    const museum = await Museum.findByIdAndUpdate(id, updatedData);
 
     if (!museum) {
       return res.status(404).json({ msg: "Museo no encontrado" });
     }
 
-    (museum.name = name),
-      (museum.description = description),
-      (museum.image = image),
-      (museum.category = category),
-      (museum.coordinates = {
-        lat: coordinates.lat,
-        lng: coordinates.lng,
-      }),
-      (museum.address = {
-        streetAddress: address.streetAddress,
-        addressLocality: address.addressLocality,
-        postalCode: address.postalCode,
-        addressCountry: address.addressCountry,
-      }),
-      (museum.telephone = telephone),
-      (museum.url = url),
-      (museum.openingHours = [...openingHours]),
-      (museum.priceRange = {
-        freeSunday: priceRange.freeSunday,
-        general: priceRange.general,
-        student: priceRange.student,
-        inapam: priceRange.inapam,
-      }),
-      (museum.review = {
-        reviewRating: {
-          ratingValue: review.reviewRating.ratingValue,
-          bestRating: review.reviewRating.bestRating,
-          worstRating: review.reviewRating.worstRating,
-          author: review.reviewRating.author,
-          datePublished: review.reviewRating.datePublished,
-          description: review.reviewRating.description,
-        },
-      }),
-      (museum.travelTime = travelTime),
-      (museum.stayTime = stayTime),
-      await museum.save();
-
     return res.status(200).json({ msg: "Museo actualizado" });
   } catch (error) {
-    if (error.kind === "ObjectId")
-      return res.status(404).json({ msg: "Formato de id incorrecto" });
-
-    return res.status(500).json({ msg: "Ocurrio un error en el servidor" });
+    if (error.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Formato de ID incorrecto" });
+    } else {
+      return res.status(500).json({ msg: "Ocurrió un error en el servidor" });
+    }
   }
 };
 
@@ -235,7 +185,6 @@ export const deleteMuseumById = async (req, res) => {
 export const getCategoriesWithCounts = async (req, res) => {
   try {
     const categories = await Museum.distinct("category");
-
     const categoryCounts = await Museum.aggregate([
       {
         $group: {
